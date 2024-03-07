06614643b4e410e133c0c22d08da47ed9204d14f
kharle14 <enriquezkharl14@gmail.com>
2024-03-07 16:41:48 +0800

    [Feat] Forgot password form validation

--- src/features/auth/ForgotPasswordForm.tsx
+++ src/features/auth/ForgotPasswordForm.tsx
@@ -1,20 +1,42 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useAuthContext } from '@/contexts/authContext';
+import { ForgotPasswordSchema, ForgotPasswordSchemaType } from '@/schemas/auth';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Label } from '@radix-ui/react-label';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 export default function ForgotPasswordForm() {
   const { handleState } = useAuthContext();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ForgotPasswordSchemaType>({ resolver: zodResolver(ForgotPasswordSchema) });
+
+  const onSubmit: SubmitHandler<ForgotPasswordSchemaType> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form autoComplete="off">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      autoComplete="off"
+    >
       <div className="space-y-2">
         <div>
           <Label htmlFor="email">Email</Label>
           <Input
             id="email"
-            name="email"
+            {...register('email')}
           />
+          <Label
+            htmlFor="email"
+            className="text-xs text-red-500"
+          >
+            {errors.email?.message}
+          </Label>
         </div>
       </div>
 
