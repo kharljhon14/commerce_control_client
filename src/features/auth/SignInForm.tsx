2a908f997d8c5d567a43f5f75f228c720406f38c
kharle14 <enriquezkharl14@gmail.com>
2024-03-14 21:11:39 +0800

    [Feat] Added alert message on sign up error

--- src/features/auth/SignInForm.tsx
+++ src/features/auth/SignInForm.tsx
@@ -10,8 +10,12 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert';
+import { useState } from 'react';
 
 export default function SignInForm() {
+  const [errorMessage, setErrorMessage] = useState('');
+
   const router = useRouter();
   const {
     register,
@@ -22,6 +26,7 @@ export default function SignInForm() {
   });
 
   const onSubmit: SubmitHandler<SignInSchemaType> = async (data) => {
+    setErrorMessage('');
     const res = await fetch('http://localhost:8000/auth/sign-in', {
       method: 'POST',
       headers: {
@@ -32,7 +37,7 @@ export default function SignInForm() {
 
     if (!res.ok) {
       const error = await res.json();
-      alert(error.message);
+      setErrorMessage(error.message);
 
       return;
     }
@@ -45,64 +50,73 @@ export default function SignInForm() {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      autoComplete="off"
-    >
-      <div className="space-y-2">
-        <div>
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            errorMessage={errors.email?.message}
-            {...register('email')}
-          />
-          <Label
-            htmlFor="email"
-            className="text-xs text-red-500"
-          >
-            {errors.email?.message}
-          </Label>
-        </div>
+    <div className="space-y-4">
+      {errorMessage && (
+        <Alert variant="destructive">
+          <AlertTitle>Something went wrong!</AlertTitle>
+          <AlertDescription>{errorMessage}</AlertDescription>
+        </Alert>
+      )}
 
-        <div>
-          <Label htmlFor="password">Password</Label>
-          <PasswordInput
-            id="password"
-            errorMessage={errors.password?.message}
-            register={register('password')}
-          />
-          <Label
-            htmlFor="password"
-            className="text-xs text-red-500"
-          >
-            {errors.password?.message}
-          </Label>
-        </div>
-      </div>
-
-      <div className="flex justify-between mx-4 mt-2">
-        <Link
-          href="/auth/forgot-password"
-          className="text-blue-500 p-0"
-        >
-          Forgot Password
-        </Link>
-
-        <Link
-          href="/auth/sign-up"
-          className="text-blue-500 p-0"
-        >
-          Sign Up
-        </Link>
-      </div>
-
-      <Button
-        className="w-full mt-6"
-        size="lg"
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        autoComplete="off"
       >
-        Submit
-      </Button>
-    </form>
+        <div className="space-y-2">
+          <div>
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              errorMessage={errors.email?.message}
+              {...register('email')}
+            />
+            <Label
+              htmlFor="email"
+              className="text-xs text-red-500"
+            >
+              {errors.email?.message}
+            </Label>
+          </div>
+
+          <div>
+            <Label htmlFor="password">Password</Label>
+            <PasswordInput
+              id="password"
+              errorMessage={errors.password?.message}
+              register={register('password')}
+            />
+            <Label
+              htmlFor="password"
+              className="text-xs text-red-500"
+            >
+              {errors.password?.message}
+            </Label>
+          </div>
+        </div>
+
+        <div className="flex justify-between mx-4 mt-2">
+          <Link
+            href="/auth/forgot-password"
+            className="text-blue-500 p-0"
+          >
+            Forgot Password
+          </Link>
+
+          <Link
+            href="/auth/sign-up"
+            className="text-blue-500 p-0"
+          >
+            Sign Up
+          </Link>
+        </div>
+
+        <Button
+          className="w-full mt-6"
+          size="lg"
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
   );
 }
